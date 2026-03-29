f8e845916e28dd24d2c7e4d23591bf515d0f9d6c
Bram <brampeer15@gmail.com>
2026-03-29 18:47:43 +0200

    Able to start project with Docker

--- backend-api/src/main/java/com/brampeerdeman/api/ApiApplication.java
+++ backend-api/src/main/java/com/brampeerdeman/api/ApiApplication.java
@@ -2,10 +2,30 @@ package com.brampeerdeman.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter; // LET OP: Deze import moet van Spring zijn!
 
 @SpringBootApplication
 public class ApiApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("http://localhost:5173");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+
+		source.registerCorsConfiguration("/**", config);
+		// Geef de 'source' mee aan de constructor!
+		return new CorsFilter(source);
 	}
 }
